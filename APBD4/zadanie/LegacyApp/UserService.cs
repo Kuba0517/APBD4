53ba2f176c09cec7250feb37a625161ec42de467
Jakub <jakub.ratusz@gmail.com>
2024-03-29 14:47:10 +0100

    Refactor the way credit limit is assigned

--- APBD4/zadanie/LegacyApp/UserService.cs
+++ APBD4/zadanie/LegacyApp/UserService.cs
@@ -27,29 +27,7 @@ namespace LegacyApp
 
             var user = CreateNewUser(client, dateOfBirth, email, firstName, lastName);
 
-
-            if (client.Type == "VeryImportantClient")
-            {
-                user.HasCreditLimit = false;
-            }
-            else if (client.Type == "ImportantClient")
-            {
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
-            else
-            {
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
-            }
+            AssignCreditLimit(user);
 
             if (user.HasCreditLimit && user.CreditLimit < 500)
             {
@@ -97,6 +75,32 @@ namespace LegacyApp
             };
         }
         
-        
+        private void AssignCreditLimit(User user)
+        {
+            var client = user.Client;
+            switch (((Client)client).Type)
+            {
+                case "VeryImportantClient":
+                    user.HasCreditLimit = false;
+                    break;
+                case "ImportantClient":
+                    user.HasCreditLimit = true;
+                    using (var userCreditService = new UserCreditService())
+                    {
+                        int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                        creditLimit = creditLimit * 2;
+                        user.CreditLimit = creditLimit;
+                    }
+                    break;
+                default:
+                    user.HasCreditLimit = true;
+                    using (var userCreditService = new UserCreditService())
+                    {
+                        int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                        user.CreditLimit = creditLimit;
+                    }
+                    break;
+            }
+        }
     }
 }
